ed704cf0e8351fb0c55f884bf06ed83c10ccf4eb
Shane Solomon <shsolomo@microsoft.com>
2019-03-20 00:18:33 -0700

    Added Function and variable practice.

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
+void multBy2(int *var2)
+{
+    *var2 = *var2 * 2;
+
+}
+
+int practice()
+{
+    ///Pointers
+    int var1;
+    printf("The address of var1 is %x \n", &var1);
+
+    int var3 = 10;
+    multBy2(&var1);
+    printf("var1 = %d \n", var1);
+
+    ///Data types and variables
+    float x = 12.345678;
+    printf("The value of x is %0.5f \n", x);
+    
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     printf("connected\n");
+
+    practice();
 
     return 0;
 }
